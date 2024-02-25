943be11d8fc111d39873be83536bb98abd21e58d
AgustinhoIA <agustinhoigal@gmail.com>
2024-02-25 17:52:40 -0300

    Add basic html and css selectors

--- library.js
+++ library.js
@@ -13,7 +13,7 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(newBook)
 }
 
-addBookToLibrary("namw fo the wind", "pat ruff", "687", true);
+addBookToLibrary("name fo the wind", "pat ruff", "687", true);
 addBookToLibrary("doors of stone", "pat ruf", "1687", false)
 
 console.log(myLibrary[0].title)
