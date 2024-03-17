2dd2d9058443411cd953e1f08e7b4c9af62a7e3c
AgustinhoIA <agustinhoigal@gmail.com>
2024-03-17 16:14:49 -0300

    Add addNewBook function + minor corrections to css

--- library.js
+++ library.js
@@ -1,16 +1,29 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read) { /* */ 
  this.title = title;
  this.author = author;
  this.pages = pages;
- this.read = read || false;
+ this.read = read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
 
   myLibrary.push(newBook)
+}
+
+function AddNewBook(event) {
+  event.preventDefault(); 
+
+  // Access form data
+  var subAuthor = document.getElementById('Author').value;
+  var subTitle = document.getElementById('Title').value;
+  var subLenght = document.getElementById('Lenght').value;
+  var subRead = document.getElementById('read').checked;
+
+  // Call your function with the form data
+  addBookToLibrary(subTitle, subAuthor, subLenght, subRead);
 }
 
 addBookToLibrary("name fo the wind", "pat ruff", "687", true);
