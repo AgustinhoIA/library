23ff672979c3c15762c4198106c9be3891436b72
AgustinhoIA <agustinhoigal@gmail.com>
2024-03-17 16:38:44 -0300

    remove test code

--- library.js
+++ library.js
@@ -11,23 +11,20 @@ function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
 
   myLibrary.push(newBook)
+  console.log(myLibrary)
 }
 
 function NewBook(event) {
   event.preventDefault(); 
 
   // Access form data
-  var subAuthor = document.getElementById('Author').value;
-  var subTitle = document.getElementById('Title').value;
-  var subLenght = document.getElementById('Lenght').value;
+  var subAuthor = document.getElementById('author').value;
+  var subTitle = document.getElementById('title').value;
+  var subLenght = document.getElementById('pages').value;
   var subRead = document.getElementById('read').checked;
 
   // Call your function with the form data
   addBookToLibrary(subTitle, subAuthor, subLenght, subRead);
 }
 
-addBookToLibrary("name fo the wind", "pat ruff", "687", true);
-addBookToLibrary("doors of stone", "pat ruf", "1687", false)
-
-console.log(myLibrary[0].title)
 document.getElementById('addBookForm').addEventListener('submit', NewBook);
